adbea7b9202ce9d45c7560a45f67bf15008c0247
BAMOGO <clement_bamogo@um5.ac.ma>
2024-01-04 19:49:48 +0100

    m

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,20 @@ import { Route, Routes } from "react-router-dom";
 import AppBar from "./components/appBar/appBar";
 import "./App.scss"
 import Home from "./components/home/index";
-import Layout from "./components/layout/layout";
 import About from "./components/about/index";
 import Works from "./components/work/index";
 import Error from "./components/error";
+import Layout from "./components/layout/layout";
 function App() {
   return (
     <>
-      <AppBar></AppBar>
+      {/* <AppBar></AppBar> */}
       <Routes>
-          {/* <Route path='/' index element={<Layout />} /> */}
-          <Route path='/' index element={<Home />} />
-          <Route path='/work' index element={<Works />} />
-          <Route path='/about' index element={<About />} />
-          <Route path='/error' index element={<Error />} />
+        <Route path='/' index element={<Layout />} />
+        {/* <Route path='/' index element={<Home />} />
+        <Route path='/work' element={<Works />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/error' element={<Error />} /> */}
       </Routes>
     </>
   );
