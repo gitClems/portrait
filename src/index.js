eb4d1349d3a73c0b5dc9b447b1e22e56950c82f2
BAMOGO <clement_bamogo@um5.ac.ma>
2024-01-04 22:22:12 +0100

    m

--- src/index.js
+++ src/index.js
@@ -2,14 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
+import AppBar from './components/appBar/appBar';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <>
+      {/* <AppBar></AppBar> */}
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </>
   </React.StrictMode>
 );
 
