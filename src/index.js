a49d451dc4518d692108abb42a0b08c3d6082898
BAMOGO <clement_bamogo@um5.ac.ma>
2024-01-05 07:25:32 +0100

    deploy

--- src/index.js
+++ src/index.js
@@ -2,14 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <HashRouter>
+    <BrowserRouter>
         <App />
-      </HashRouter>
+    </BrowserRouter>
+      {/* <HashRouter>
+      </HashRouter> */}
   </React.StrictMode>
 );
 
