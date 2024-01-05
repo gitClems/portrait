2b9c37e98df7df117d32af00f47bc5d3ee0de223
BAMOGO <clement_bamogo@um5.ac.ma>
2024-01-05 09:03:27 +0100

    deploy

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -24,33 +24,33 @@ export const PreloadImages = ({ imageUrls, children }) => {
                 setImagesLoaded(true); // Set to true to avoid infinite loading if an error occurs
             });
     }, [imageUrls]);
-    return <div>{imagesLoaded ? children : <p>Loading images...</p>}</div>;
+    return <div>{imagesLoaded ? children : <p style={{ height: 50, width: 100, backgroundColor: 'red' }}>Loading images...</p>}</div>;
 }
 
 const Hero = (props) => {
 
     return (
-        <>
-            <div className="hero" >
-                <div className="accroche">
-                    <div className="me">
-                        <p className="cl-hi" >Bonjour,</p>
-                        <p className="cl-name" >Je m'appelle {props.name}</p>
-                        <p className="cl-what" >{props.description}</p>
+        <PreloadImages imageUrls={props.profile}>
+            <>
+                <div className="hero" >
+                    <div className="accroche">
+                        <div className="me">
+                            <p className="cl-hi" >Bonjour,</p>
+                            <p className="cl-name" >Je m'appelle {props.name}</p>
+                            <p className="cl-what" >{props.description}</p>
+                        </div>
+                        <div className="tools">
+                            <div className="tool"><FontAwesomeIcon icon={faWarehouse} />SCM</div>
+                            <div className="tool"><FontAwesomeIcon icon={faChartSimple} />BI</div>
+                            <div className="tool"><FontAwesomeIcon icon={faCode} />Dev</div>
+                        </div>
                     </div>
-                    <div className="tools">
-                        <div className="tool"><FontAwesomeIcon icon={faWarehouse} />SCM</div>
-                        <div className="tool"><FontAwesomeIcon icon={faChartSimple} />BI</div>
-                        <div className="tool"><FontAwesomeIcon icon={faCode} />Dev</div>
-                    </div>
-                </div>
-                <div className="profile">
-                    <PreloadImages imageUrls = {props.profile}>
+                    <div className="profile">
                         <img src={props.profile} alt="Mon profile" width={350} />
-                    </PreloadImages>
+                    </div>
                 </div>
-            </div>
-        </>
+            </>
+        </PreloadImages>
     )
 }
 
